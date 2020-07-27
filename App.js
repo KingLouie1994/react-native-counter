a38bc5d28cee25a47a44e4d37b533c10efddb8c4
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-07-27 16:36:10 +0200

    first try of using react native

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  function changer() {
+    setCounter((prevCount) => prevCount + 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.countContainer}>
+        <Text>Counter: {counter}</Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={changer}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +24,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: "orange",
+    borderColor: "orange",
+    borderRadius: 5,
   },
 });
